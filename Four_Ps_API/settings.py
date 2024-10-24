ebac0d0fb606fa29e695a14cd100cd553b8fd88a
Acacius03 <felix.e.g.deloyola@gmail.com>
2024-10-24 21:42:32 +0800

    edit cors

--- Four_Ps_API/settings.py
+++ Four_Ps_API/settings.py
@@ -36,13 +36,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # external
     'rest_framework',
+    # internal
     'api',
+]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173/',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
